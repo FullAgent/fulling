d10b38aed24a9ca0e17477cedf5925377b8cbd02
Che <30403707+Che-Zhu@users.noreply.github.com>
2026-06-23 18:15:09 +0800

    refactor: export sealos auth integration

---
+++ lib/platform/integrations/sealos/auth/index.ts
@@ -0,0 +1,3 @@
+export { detectSealosIframe } from './detect-sealos-iframe'
+export { getSealosSession } from './get-sealos-session'
+export type { SealosAuthSession, SealosUserInfo } from './types'
